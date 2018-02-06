b95e76bbe6033bf943e9329badb7e057f3e17fa5
Bas van Stein <bas9112@gmail.com>
2018-02-06 16:32:29 +0100

    Fixed some typos and wrongly stated sklearn requirement.

--- BayesOpt/README.rst
+++ BayesOpt/README.rst
@@ -3,7 +3,7 @@ Bayesian Optimization Library
 
 The bayesian optimization algorithm is a surrogate-based optimizer that can
 optimize expensive black-box functions. This implementation is specifically
-tuned to optimize deel neural networks. It is able to handle paralell 
+tuned to optimize deep neural networks. It is able to handle parallel 
 evaluations on multiple GPUs, and can use a Random Forest surrogate model.
 
 For additional details see our paper: <https://coming_soon>`_.
--- BayesOpt/setup.py
+++ BayesOpt/setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.1',  # Required
+    version='0.9.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -119,7 +119,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandas','numpy','scipy','sklearn'],  # Optional
+    install_requires=['pandas','numpy','scipy','scikit-learn'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
