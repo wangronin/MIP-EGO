157dfbc7555c2fa32bfa776bb80c6623b202367c
Bas van Stein <bas9112@gmail.com>
2021-06-08 16:22:23 +0200

    Released version 1.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'readme-pypi.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
@@ -53,6 +53,9 @@ setup(
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
     long_description=long_description,  # Optional
+
+    description_content_type='text/markdown',
+    long_description_content_type='text/markdown',
 
     # This should be a valid link to your project's main homepage.
     #
