7966cbe10358ce25159a899d70a43b56a49e4307
Bas van Stein <steinbvan@liacs.leidenuniv.nl>
2021-11-25 15:14:40 +0000

    removed from init

--- mipego/GaussianProcess/__init__.py
+++ mipego/GaussianProcess/__init__.py
@@ -8,6 +8,5 @@ Created on Fri Nov 13 20:45:21 2015
 from .utils import SMSE, MSLL
 from .gpr import GaussianProcess
 from .gpy import PytorchGaussianProcess
-from .gpy_autoencoder import AutoencoderGaussianProcess
 
-__all__ = ['SMSE', 'MSLL', 'GaussianProcess', 'AutoencoderGaussianProcess', 'PytorchGaussianProcess']
+__all__ = ['SMSE', 'MSLL', 'GaussianProcess', 'PytorchGaussianProcess']
